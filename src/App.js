51967a94d7629e534e338b3e8ef76b69b99a6078
Rodrigo Soares Fragozo <rodrigo.fragozo@gmail.com>
2019-04-30 15:17:11 +0200

    add method added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Todos from './Todos';
+import AddTodo from './AddTodo';
 
 class App extends Component {
   state = {
@@ -17,10 +18,19 @@ class App extends Component {
     console.log("todo deleted: "+ id);
   }
 
+  add = (todo) => {
+    todo.id = Math.random();
+    let todos = [...this.state.todos, todo];
+    this.setState({
+      todos
+    });
+  }
+
   render() {
     return (
       <div className="todo-app container">
         <h1 className="center blue-text">TODO LIST</h1>
+        <AddTodo addMethod={ this.add } />
         <Todos todos={this.state.todos} deleteMethod={ this.delete } />
       </div>
     );
